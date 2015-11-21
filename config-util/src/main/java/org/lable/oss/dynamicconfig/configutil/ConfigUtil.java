27da5752d5073beae58a790add3bd4cc33e869f1
Jeroen Hoek <jeroen.hoek@lable.nl>
2015-11-21 13:33:35 +0100

    Add a utility module, config-util
    Contains static helper methods for working with Apache Commons Configuration.

---
+++ config-util/src/main/java/org/lable/oss/dynamicconfig/configutil/ConfigUtil.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (C) 2015 Lable (info@lable.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lable.oss.dynamicconfig.configutil;
+
+import org.apache.commons.configuration.Configuration;
+
+import java.util.*;
+
+/**
+ * Static convenience methods for working with {@link Configuration} instances.
+ */
+public class ConfigUtil {
+    ConfigUtil() {
+        // No-op.
+    }
+
+    /**
+     * Get a map of configuration nodes mapped to the names of the children of the parent parameter.
+     *
+     * @param config Configuration object, may be a subset of the configuration tree.
+     * @param parent Path to the parent node.
+     * @return A map of all the children for the parent configuration node passed.
+     */
+    public static Map<String, Configuration> childMap(Configuration config, String parent) {
+        if (config == null) throw new IllegalArgumentException("Parameter config may not be null.");
+
+        if (parent != null && !parent.isEmpty()) {
+            config = config.subset(parent);
+        }
+
+        Iterator<String> keys = config.getKeys();
+        Map<String, Configuration> map = new LinkedHashMap<>();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            key = key.split("\\.", 2)[0];
+            if (!map.containsKey(key)) {
+                map.put(key, config.subset(key));
+            }
+        }
+        return map;
+    }
+
+    /**
+     * Get a map of configuration nodes mapped to the names of the children of the root configuration node.
+     *
+     * @param config Configuration object, may be a subset of the configuration tree.
+     * @return A map of all the children for the parent configuration node passed.
+     */
+    public static Map<String, Configuration> childMap(Configuration config) {
+        return childMap(config, null);
+    }
+
+    /**
+     * Get the list of child keys for a configuration node.
+     *
+     * @param config Configuration object, may be a subset of the configuration tree.
+     * @param parent Path to the parent node.
+     * @return A string array containing the names of the children.
+     */
+    public static List<String> childKeys(Configuration config, String parent) {
+        if (config == null) throw new IllegalArgumentException("Parameter config may not be null.");
+
+        if (parent != null && !parent.isEmpty()) {
+            config = config.subset(parent);
+        }
+
+        Iterator keys = config.getKeys();
+        List<String> list = new ArrayList<String>();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            key = key.split("\\.", 2)[0];
+            list.add(key);
+        }
+        return list;
+    }
+
+
+    /**
+     * Get the list of child keys for a configuration node.
+     *
+     * @param config Configuration object, may be a subset of the configuration tree.
+     * @return A string array containing the names of the children.
+     */
+    public static List<String> childKeys(Configuration config) {
+        return childKeys(config, null);
+    }
+}
---
+++ config-util/src/test/java/org/lable/oss/dynamicconfig/configutil/ConfigUtilTest.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (C) 2015 Lable (info@lable.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lable.oss.dynamicconfig.configutil;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.lable.oss.dynamicconfig.configutil.ConfigUtil.childKeys;
+import static org.lable.oss.dynamicconfig.configutil.ConfigUtil.childMap;
+
+public class ConfigUtilTest {
+    @Test
+    public void testChildMapReturnsMap() {
+        Configuration input = new HierarchicalConfiguration();
+        input.addProperty("prop1", "XXX");
+        input.addProperty("prop2.l1", "XXX");
+        input.addProperty("prop2.l2", "XXX");
+        input.addProperty("prop3.deeper", "XXX");
+
+        final Map<String, Configuration> output = childMap(input);
+
+        assertThat(output.size(), is(3));
+        assertThat(output.get("prop3").getString("deeper"), is("XXX"));
+    }
+
+    @Test
+    public void testChildMapReturnsSubsetMap() {
+        Configuration input = new HierarchicalConfiguration();
+        input.addProperty("prop1", "XXX");
+        input.addProperty("prop2.l1", "XXX");
+        input.addProperty("prop2.l2", "XXX");
+        input.addProperty("prop3.deeper", "XXX");
+
+        final Map<String, Configuration> output = childMap(input.subset("prop2"));
+
+        assertThat(output.size(), is(2));
+        assertThat(output.containsKey("l1"), is(true));
+        assertThat(output.containsKey("l2"), is(true));
+    }
+
+    @Test
+    public void testChildMapReturnsSubsetMapWithPath() {
+        Configuration input = new HierarchicalConfiguration();
+        input.addProperty("prop1", "XXX");
+        input.addProperty("prop2.l1", "XXX");
+        input.addProperty("prop2.l2", "XXX");
+        input.addProperty("prop3.deeper", "XXX");
+
+        final Map<String, Configuration> output = childMap(input, "prop2");
+
+        assertThat(output.size(), is(2));
+        assertThat(output.containsKey("l1"), is(true));
+        assertThat(output.containsKey("l2"), is(true));
+    }
+
+    @Test
+    public void testChildMapEmptyPath() {
+        Configuration input = new HierarchicalConfiguration();
+        input.addProperty("prop1", "XXX");
+
+        final Map<String, Configuration> output = childMap(input, "");
+
+        assertThat(output.containsKey("prop1"), is(true));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testChildMapRefusesNull() {
+        childMap(null, null);
+    }
+
+
+    @Test
+    public void testChildKeys() {
+        Configuration input = new HierarchicalConfiguration();
+        input.addProperty("prop1", "XXX");
+        input.addProperty("prop2", "XXX");
+        input.addProperty("prop3.deeper", "XXX");
+
+        final List<String> result = childKeys(input);
+
+        assertThat(result.size(), is(3));
+        assertThat(result.get(0), is("prop1"));
+        assertThat(result.get(1), is("prop2"));
+        assertThat(result.get(2), is("prop3"));
+    }
+
+    @Test
+    public void testChildKeysSubList() {
+        Configuration input = new HierarchicalConfiguration();
+        input.addProperty("prop1", "XXX");
+        input.addProperty("prop2", "XXX");
+        input.addProperty("prop3.deeper", "XXX");
+
+        final List<String> result = childKeys(input, "prop3");
+
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0), is("deeper"));
+    }
+
+    @Test
+    public void testChildKeysEmptyPath() {
+        Configuration input = new HierarchicalConfiguration();
+        input.addProperty("prop1", "XXX");
+
+        final List<String> result = childKeys(input, "");
+
+        assertThat(result.contains("prop1"), is(true));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testChildKeysRefusesNull() {
+        childKeys(null);
+    }
+
+
+    @Test
+    public void codeCoverageTest() {
+        new ConfigUtil();
+    }
+}
