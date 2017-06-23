c4409bcf9bc9a0accf31d79bb179c82da04aa914
Taco Jan Osinga <taco.jan.osinga@lable.nl>
2017-06-23 15:24:46 +0200

    Add metadata to InstanceLocalSettings

--- core/src/main/java/org/lable/oss/dynamicconfig/core/InstanceLocalSettings.java
+++ core/src/main/java/org/lable/oss/dynamicconfig/core/InstanceLocalSettings.java
@@ -15,13 +15,25 @@
  */
 package org.lable.oss.dynamicconfig.core;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 /**
  * Process-local settings for bootstrapping this library.
  */
 public enum InstanceLocalSettings {
     INSTANCE;
 
-    private String name = null;
+    private String name;
+    private Map<MetaDataKey, String> metaData;
+
+    InstanceLocalSettings() {
+        name = null;
+        metaData = new HashMap<>();
+    }
 
     public void setAppName(String name) {
         this.name = name;
@@ -29,5 +41,66 @@ public enum InstanceLocalSettings {
 
     public String getAppName() {
         return name;
+    }
+
+    public void setMetaData(String category, String key, String value) {
+        metaData.put(new MetaDataKey(category, key), value);
+    }
+
+    public Optional<String> getMetaData(String category, String key) {
+        MetaDataKey mdk = new MetaDataKey(category, key);
+        return Optional.ofNullable(metaData.get(mdk));
+    }
+
+    public Map<MetaDataKey, String> getMetaData(String category) {
+        return metaData.entrySet().stream()
+                .filter((entry) -> entry.getKey().getCategory().equals(category))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    public Map<MetaDataKey, String> getMetaData() {
+        return metaData;
+    }
+
+    /**
+     * Category-key combination that is used as key for meta data in the InstanceLocalSettings.
+     */
+    public static class MetaDataKey {
+
+        private String category;
+        private String key;
+
+        MetaDataKey(String category, String key) {
+            if (category == null) throw new IllegalArgumentException("Category may not be null.");
+            if (key == null) throw new IllegalArgumentException("Key may not be null.");
+
+            this.category = category;
+            this.key = key;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(category, key);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (!(other instanceof MetaDataKey)) return false;
+            MetaDataKey that = (MetaDataKey) other;
+            return Objects.equals(this.category, that.category) && Objects.equals(this.key, that.key);
+        }
+
+        @Override
+        public String toString() {
+            return category + ":" + key;
+        }
     }
 }
--- core/src/test/java/org/lable/oss/dynamicconfig/core/InstanceLocalSettingsTest.java
+++ core/src/test/java/org/lable/oss/dynamicconfig/core/InstanceLocalSettingsTest.java
@@ -16,12 +16,17 @@
 package org.lable.oss.dynamicconfig.core;
 
 import org.junit.Test;
+import org.lable.oss.dynamicconfig.core.InstanceLocalSettings.MetaDataKey;
 
+import java.util.Map;
+
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class InstanceLocalSettingsTest {
+
     @Test
     public void setAppNameTest() {
         InstanceLocalSettings.INSTANCE.setAppName("XXX");
@@ -32,4 +37,57 @@ public class InstanceLocalSettingsTest {
     public void getAppNameUnsetTest() {
         assertThat(InstanceLocalSettings.INSTANCE.getAppName(), is(nullValue()));
     }
+
+    @Test
+    public void setMetaData() {
+        InstanceLocalSettings settings = InstanceLocalSettings.INSTANCE;
+        settings.setMetaData("category1", "key1", "value1");
+        settings.setMetaData("category1", "key2", "value2");
+
+        assertThat(settings.getMetaData().size(), is(2));
+        assertThat(settings.getMetaData("category1", "key1").get(), is("value1"));
+        assertThat(settings.getMetaData("category1", "key2").get(), is("value2"));
+
+        // Overwrite meta data
+        settings.setMetaData("category1", "key1", "value3");
+
+        assertThat(settings.getMetaData().size(), is(2));
+        assertThat(settings.getMetaData("category1", "key1").get(), is("value3"));
+
+        // Add new category
+        settings.setMetaData("category2", "key1", "value1");
+
+        Map<MetaDataKey, String> cat1 = settings.getMetaData("category1");
+        Map<MetaDataKey, String> cat2 = settings.getMetaData("category2");
+        Map<MetaDataKey, String> cat3 = settings.getMetaData("does-not-exist");
+
+        assertThat(cat1.size(), is(2));
+        assertThat(cat2.size(), is(1));
+        assertThat(cat3.size(), is(0));
+    }
+
+    @Test
+    public void testMetaDataKey() {
+        MetaDataKey key1 = new MetaDataKey("same", "key");
+        MetaDataKey key2 = new MetaDataKey("same", "key");
+        MetaDataKey key3 = new MetaDataKey("different", "key");
+
+        assertThat(key1, is(key2));
+        assertThat(key1, not(key3));
+        assertThat(key2, not(key3));
+
+        assertThat(key1.toString(), is("same:key"));
+        assertThat(key3.toString(), is("different:key"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMetaDataKeyExceptionOnCategory() {
+        new MetaDataKey(null, "test");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMetaDataKeyExceptionOnKey() {
+        new MetaDataKey("test", null);
+    }
+
 }
